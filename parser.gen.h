95a0074edf1b4d6bffb8592ae39f4715561fe67c
Stephen Dolan <mu@netsoc.tcd.ie>
2012-10-21 19:52:16 +0100

    Check in a bunch of autogenerated files.
    These seem not to build with the version of bison, etc. on OS X.
    This is a hack for now, I'll fix the OS X build when I get access
    to a mac.

---
+++ parser.gen.h
@@ -0,0 +1,130 @@
+/* A Bison parser, made by GNU Bison 2.5.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* "%code requires" blocks.  */
+
+/* Line 2068 of yacc.c  */
+#line 6 "parser.y"
+
+#include "locfile.h"
+#define YYLTYPE location
+#define YYLLOC_DEFAULT(Loc, Rhs, N)             \
+  do {                                          \
+    if (N) {                                    \
+      (Loc).start = YYRHSLOC(Rhs, 1).start;     \
+      (Loc).end = YYRHSLOC(Rhs, N).end;         \
+    } else {                                    \
+      (Loc).start = YYRHSLOC(Rhs, 0).end;       \
+      (Loc).end = YYRHSLOC(Rhs, 0).end;         \
+    }                                           \
+  } while (0)
+ 
+
+
+/* Line 2068 of yacc.c  */
+#line 55 "parser.gen.h"
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     INVALID_CHARACTER = 258,
+     IDENT = 259,
+     LITERAL = 260,
+     EQ = 261,
+     DEFINEDOR = 262,
+     AS = 263,
+     DEF = 264,
+     IF = 265,
+     THEN = 266,
+     ELSE = 267,
+     ELSE_IF = 268,
+     END = 269,
+     AND = 270,
+     OR = 271,
+     SETPIPE = 272,
+     SETPLUS = 273,
+     SETMINUS = 274,
+     SETMULT = 275,
+     SETDIV = 276,
+     SETDEFINEDOR = 277,
+     LESSEQ = 278,
+     GREATEREQ = 279,
+     QQSTRING_START = 280,
+     QQSTRING_TEXT = 281,
+     QQSTRING_INTERP_START = 282,
+     QQSTRING_INTERP_END = 283,
+     QQSTRING_END = 284
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 2068 of yacc.c  */
+#line 24 "parser.y"
+
+  jv literal;
+  block blk;
+
+
+
+/* Line 2068 of yacc.c  */
+#line 108 "parser.gen.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+
+
