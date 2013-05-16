919292764a1ba57507ab4c93e732d2037a2ffa14
Stephen Dolan <mu@netsoc.tcd.ie>
2013-05-16 14:25:28 +0100

    Reverse function. Closes #94.

--- builtin.c
+++ builtin.c
@@ -562,6 +562,7 @@ static const char* const jq_builtins[] = {
   "def to_entries: [keys[] as $k | {key: $k, value: .[$k]}];",
   "def from_entries: map({(.key): .value}) | add;",
   "def with_entries(f): to_entries | map(f) | from_entries;",
+  "def reverse: [.[length - 1 - range(0;length)]];",
 };
 
 
