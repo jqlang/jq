bbf4e05fde301bc48907ca6d485c59ae10a6467c
Nicolas Williams <nico@cryptonector.com>
2013-06-15 00:08:59 -0500

    Move slurp_file() into library as jv_load_file()
    Needed as part of creating a libjq.

---
+++ jv_file.c
@@ -0,0 +1,45 @@
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "jv.h"
+#include "jv_aux.h"
+#include "jv_parse.h"
+
+jv jv_load_file(const char* filename, int raw) {
+  FILE* file = fopen(filename, "r");
+  struct jv_parser parser;
+  jv data;
+  if (!file) {
+    return jv_invalid_with_msg(jv_string_fmt("Could not open %s: %s",
+                                             filename,
+                                             strerror(errno)));
+  }
+  if (raw) {
+    data = jv_string("");
+  } else {
+    data = jv_array();
+    jv_parser_init(&parser);
+  }
+  while (!feof(file) && !ferror(file)) {
+    char buf[4096];
+    size_t n = fread(buf, 1, sizeof(buf), file);
+    if (raw) {
+      data = jv_string_concat(data, jv_string_sized(buf, (int)n));
+    } else {
+      jv_parser_set_buf(&parser, buf, strlen(buf), !feof(file));
+      jv value;
+      while (jv_is_valid((value = jv_parser_next(&parser))))
+        data = jv_array_append(data, value);
+    }
+  }
+  int badread = ferror(file);
+  fclose(file);
+  if (badread) {
+    jv_free(data);
+    return jv_invalid_with_msg(jv_string_fmt("Error reading from %s",
+                                             filename));
+  }
+  return data;
+}
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "compile.h"
-
-jv slurp_file(const char*, int);
-
-
-#endif
