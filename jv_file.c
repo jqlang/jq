e005ec362835d0e15214f3fb7d78273b16933e5e
Nicolas Williams <nico@cryptonector.com>
2013-06-15 17:50:40 -0500

    Always set O_CLOEXEC in an MT-safe way

--- jv_file.c
+++ jv_file.c
@@ -7,8 +7,44 @@
 #include "jv_aux.h"
 #include "jv_parse.h"
 
+static FILE *my_fopen_re(const char *filename)
+{
+#if defined(O_CLOEXEC) || defined(FD_CLOEXEC)
+  int fd;
+
+#ifdef O_CLOEXEC
+  fd = open(filename, O_RDONLY | O_CLOEXEC);
+  if (fd == -1)
+    return NULL;
+#else
+  /*
+   * This is not MT-safe, but hopefully any thread doing
+   * [v]fork()+exec*() will closefrom() or equivalent.
+   */
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+    return NULL;
+  long flags = fcntl(fd, F_GETFD) | FD_CLOEXEC;
+  fcntl(fd, FD_SETFD, flags);
+#endif /* O_CLOEXEC */
+
+  f = fdopen(fd, "r");
+  if (!f)
+    close(fd);
+  return f;
+#else
+  FILE *f = fopen(filename, "re");
+
+  if (f)
+      return f;
+
+  return fopen(filename, "r");
+#endif
+}
+
+
 jv jv_load_file(const char* filename, int raw) {
-  FILE* file = fopen(filename, "r");
+  FILE* file = my_fopen_re(filename);
   struct jv_parser parser;
   jv data;
   if (!file) {
