8d4748c8df283d06e58ac6202505057e99a4a83a
Stephen Dolan <mu@netsoc.tcd.ie>
2013-11-07 12:18:48 +0000

    Fix a crash on group_by of empty list. Fixes #208.

--- jv_aux.c
+++ jv_aux.c
@@ -545,21 +545,23 @@ jv jv_group(jv objects, jv keys) {
   int n = jv_array_length(jv_copy(objects));
   struct sort_entry* entries = sort_items(objects, keys);
   jv ret = jv_array();
-  jv curr_key = entries[0].key;
-  jv group = jv_array_append(jv_array(), entries[0].object);
-  for (int i = 1; i < n; i++) {
-    if (jv_equal(jv_copy(curr_key), jv_copy(entries[i].key))) {
-      jv_free(entries[i].key);
-    } else {
-      jv_free(curr_key);
-      curr_key = entries[i].key;
-      ret = jv_array_append(ret, group);
-      group = jv_array();
+  if (n > 0) {
+    jv curr_key = entries[0].key;
+    jv group = jv_array_append(jv_array(), entries[0].object);
+    for (int i = 1; i < n; i++) {
+      if (jv_equal(jv_copy(curr_key), jv_copy(entries[i].key))) {
+        jv_free(entries[i].key);
+      } else {
+        jv_free(curr_key);
+        curr_key = entries[i].key;
+        ret = jv_array_append(ret, group);
+        group = jv_array();
+      }
+      group = jv_array_append(group, entries[i].object);
     }
-    group = jv_array_append(group, entries[i].object);
+    jv_free(curr_key);
+    ret = jv_array_append(ret, group);
   }
-  jv_free(curr_key);
-  ret = jv_array_append(ret, group);
   jv_mem_free(entries);
   return ret;
 }
