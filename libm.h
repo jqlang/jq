59302fd2086b2bcc7c33e40bede320c2623eafac
Nicolas Williams <nico@cryptonector.com>
2014-02-21 10:49:21 -0600

    Add missing trig functions and barebones test

--- libm.h
+++ libm.h
@@ -1,7 +1,10 @@
 LIBM_DD(acos)
 LIBM_DD(acosh)
 LIBM_DD(asin)
+LIBM_DD(asinh)
+LIBM_DD(atan)
 LIBM_DD(atanh)
+LIBM_DD(cos)
 LIBM_DD(cosh)
 LIBM_DD(exp2)
 LIBM_DD(exp)
@@ -11,8 +14,11 @@ LIBM_DD(j1)
 LIBM_DD(log10)
 LIBM_DD(log2)
 LIBM_DD(log)
+LIBM_DD(sin)
 LIBM_DD(sinh)
 LIBM_DD(sqrt)
+LIBM_DD(tan)
+LIBM_DD(tanh)
 LIBM_DD(tgamma)
 LIBM_DD(y0)
 LIBM_DD(y1)
@@ -24,3 +30,4 @@ LIBM_DD(y1)
 /* LIBM_DDD(remainder) */
 /* LIBM_DDI(scalbn) */
 /* LIBM_DDIP(lgamma_r) */
+
