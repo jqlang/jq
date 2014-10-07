56344670f4aea8bedb5bbbab55c28918fa9224a8
pkoppstein <pkoppstein@gmail.com>
2014-10-06 22:51:13 -0400

    ascii_upcase/0 and ascii_downcase/0

--- builtin.c
+++ builtin.c
@@ -1155,6 +1155,12 @@ static const char* const jq_builtins[] = {
   "         exp, _repeat;"
   "     try _repeat catch if .==\"break\" then empty else . end;",
   "def inputs: repeat(_input);",
+  // # like ruby's downcase - only characters A to Z are affected
+  "def ascii_downcase:"
+  "  explode | map( if 65 <= . and . <= 90 then . + 32  else . end) | implode;",
+  // # like ruby's upcase - only characters a to z are affected
+  "def ascii_upcase:"
+  "  explode | map( if 97 <= . and . <= 122 then . - 32  else . end) | implode;",
 };
 #undef LIBM_DD
 
