ea9183de1ce20bdc6cd95bc69a1c1f7e917a766f
pkoppstein <pkoppstein@gmail.com>
2014-10-06 22:51:13 -0400

    ascii_upcase/0 and ascii_downcase/0, with documentation and test case

--- builtin.c
+++ builtin.c
@@ -1109,6 +1109,12 @@ static const char* const jq_builtins[] = {
   "  | reduce range(0; $max) as $j"
   "      ([]; . + [reduce range(0;$length) as $i ([]; . + [ $in[$i][$j] ] )] )"
 	      "  end;",
+  // # like ruby's downcase - only characters A to Z are affected
+  "def ascii_downcase:"
+  "  explode | map( if 65 <= . and . <= 90 then . + 32  else . end) | implode;",
+  // # like ruby's upcase - only characters a to z are affected
+  "def ascii_upcase:"
+  "  explode | map( if 97 <= . and . <= 122 then . - 32  else . end) | implode;",
 };
 #undef LIBM_DD
 
