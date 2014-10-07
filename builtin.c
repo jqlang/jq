3411167c03fba129d44a0e7c9699767c9e8fd5cd
William Langford <wlangfor@gmail.com>
2014-10-06 21:48:17 -0400

    Properly call onig_error_code_to_str
    Fix #585

--- builtin.c
+++ builtin.c
@@ -615,7 +615,7 @@ static jv f_match(jq_state *jq, jv input, jv regex, jv modifiers, jv testmode) {
       options, ONIG_ENCODING_UTF8, ONIG_SYNTAX_PERL_NG, &einfo);
   if (onigret != ONIG_NORMAL) {
     UChar ebuf[ONIG_MAX_ERROR_MESSAGE_LEN];
-    onig_error_code_to_str(ebuf, onigret, einfo);
+    onig_error_code_to_str(ebuf, onigret, &einfo);
     jv_free(input);
     jv_free(regex);
     return jv_invalid_with_msg(jv_string_concat(jv_string("Regex failure: "),
