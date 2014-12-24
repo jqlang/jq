1cdcff1c01ff4681f8d73ec4ba9689a702c4c17a
Nicolas Williams <nico@cryptonector.com>
2014-12-24 02:24:53 -0600

    Fix bug caused by 8ca07a0

--- jv_parse.c
+++ jv_parse.c
@@ -257,7 +257,7 @@ static pfunc found_string(struct jv_parser* p) {
         return "Invalid escape";
       }
     } else {
-      if (c < 0x001f)
+      if (c > 0 && c < 0x001f)
         return "Invalid string: control characters from U+0000 through U+001F must be escaped";
       *out++ = c;
     }

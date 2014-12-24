jqlang/jq

commit 8ca07a08c32e0ff44934907acc41bb0a889478fb

Fix #348: reject unescaped control chars

jv_parse.c
@@ -257,6 +257,8 @@ static pfunc found_string(struct jv_parser* p) {
         return "Invalid escape";
       }
     } else {
+      if (c < 0x001f)
+        return "Invalid string: control characters from U+0000 through U+001F must be escaped";
       *out++ = c;
     }
   }

commit 1cdcff1c01ff4681f8d73ec4ba9689a702c4c17a

Fix bug caused by 8ca07a0

jv_parse.c
@@ -257,7 +257,7 @@ static pfunc found_string(struct jv_parser* p) {
         return "Invalid escape";
       }
     } else {
-      if (c < 0x001f)
+      if (c > 0 && c < 0x001f)
         return "Invalid string: control characters from U+0000 through U+001F must be escaped";
       *out++ = c;
     }