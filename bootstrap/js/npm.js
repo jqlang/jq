430efcdeb8b141dd373e44d1bb0e9529920f3878
David Tolnay <dtolnay@gmail.com>
2015-08-13 01:15:58 -0700

    Pick up bootstrap 3.3.5

---
+++ bootstrap/js/npm.js
@@ -0,0 +1,1 @@
+require("../../js/transition.js");require("../../js/alert.js");require("../../js/button.js");require("../../js/carousel.js");require("../../js/collapse.js");require("../../js/dropdown.js");require("../../js/modal.js");require("../../js/tooltip.js");require("../../js/popover.js");require("../../js/scrollspy.js");require("../../js/tab.js");require("../../js/affix.js");
