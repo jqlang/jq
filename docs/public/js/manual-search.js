7032228879c988b3262921248d69551e97d93142
David Tolnay <dtolnay@gmail.com>
2015-08-20 11:14:14 -0700

    Hide manual jqplay button and navigation in print mode

--- docs/public/js/manual-search.js
+++ docs/public/js/manual-search.js
@@ -15,6 +15,6 @@ $(function() {
     var q = $value.find('.jqprogram').text().replace(/^jq /, '').replace(/^'(.+)'$/, '$1');
     var url = 'https://jqplay.org/jq?q=' + encodeURIComponent(q) +'&j=' + encodeURIComponent(j)
     var $last_tr = $value.find('tr:last');
-    $last_tr.after('<tr><th><a href="' + url + '" class="btn btn-primary btn-sm">Run</a></th><th></th></tr><tr><th></th><th></th></tr>');
+    $last_tr.after('<tr class="jqplay-btn"><th><a href="' + url + '" class="btn btn-primary btn-sm">Run</a></th><th></th></tr><tr><th></th><th></th></tr>');
   });
 });
