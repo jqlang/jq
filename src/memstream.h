c248c15a6b9251e94775fb8d2de662f8adb6e65d
Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
2016-05-28 23:13:42 -0500

    move guards

--- src/memstream.h
+++ src/memstream.h
@@ -7,21 +7,21 @@
 #ifndef MEMSTREAM_H
 #define MEMSTREAM_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdio.h>
 
 #if _POSIX_C_SOURCE < 200809L
 
-FILE *open_memstream(char **ptr, size_t *sizeloc);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#endif /* _POSIX_C_SOURCE < 200809L */
+FILE *open_memstream(char **ptr, size_t *sizeloc);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* _POSIX_C_SOURCE < 200809L */
 
 #endif // MEMSTREAM_H
 
