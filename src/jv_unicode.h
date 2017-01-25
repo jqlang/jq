712de74cf0cbe2403a16ffa3a1bc255eb092078b
William Langford <wlangfor@gmail.com>
2017-01-24 23:05:47 -0500

    Handle cut-off UTF-8 sequences when reading files
    Read additional bytes from the file to complete the UTF-8 sequence so the bytes
    in it don't get converted to U+FFFD replacement characters.

--- src/jv_unicode.h
+++ src/jv_unicode.h
@@ -1,6 +1,7 @@
 #ifndef JV_UNICODE_H
 #define JV_UNICODE_H
 
+const char* jvp_utf8_backtrack(const char* start, const char* min, int *missing_bytes);
 const char* jvp_utf8_next(const char* in, const char* end, int* codepoint);
 int jvp_utf8_is_valid(const char* in, const char* end);
 
