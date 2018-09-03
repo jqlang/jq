18ac789d6d74f198053c547fba4e8c70cb99d55a
Matt Clegg <m@cle.gg>
2018-09-03 11:58:02 +0200

    FIX if-then-else example
    "(.+)" doesn't match new line characters so the link generated for [if-then-else](https://github.com/stedolan/jq/blob/90bc29c1b544c0436ec44246e180fdbb6d6384df/docs/content/3.manual/v1.5/manual.yml#L1804) doesn't work

--- docs/public/js/manual-search.js
+++ docs/public/js/manual-search.js
@@ -44,7 +44,7 @@ $(function() {
   $.each($('.manual-example table'), function(index, value) {
     $value = $(value)
     var j = $value.find('tr:nth-child(2) td:first').text();
-    var q = $value.find('.jqprogram').text().replace(/^jq /, '').replace(/^'(.+)'$/, '$1');
+    var q = $value.find('.jqprogram').text().replace(/^jq /, '').replace(/(\r\n|\n|\r)/gm," ").replace(/^'(.+)'$/, '$1');
     var url = 'https://jqplay.org/jq?q=' + encodeURIComponent(q) +'&j=' + encodeURIComponent(j)
     var $last_tr = $value.find('tr:last');
     $last_tr.after('<tr class="jqplay-btn"><th><a href="' + url + '" class="btn btn-primary btn-sm">Run</a></th><th></th></tr><tr><th></th><th></th></tr>');
