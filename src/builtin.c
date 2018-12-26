626b1b7c7325693bb9db73837137c02b9a001434
Ricardo Constantino <wiiaboo@gmail.com>
2018-12-26 02:49:05 +0000

    builtin: add missing windows.h include
    Follow-up fix to b436156f5 because BOOL and DWORD
    are not built-in to mingw64.

--- src/builtin.c
+++ src/builtin.c
@@ -34,6 +34,9 @@ void *alloca (size_t);
 #endif
 #include <string.h>
 #include <time.h>
+#if defined(WIN32) && !defined(HAVE_SETENV)
+#include <windows.h>
+#endif
 #include "builtin.h"
 #include "compile.h"
 #include "jq_parser.h"
