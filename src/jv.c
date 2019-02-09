2660b04a731568c54eb4b91fe811d81cbbf3470b
Nicolas Williams <nico@cryptonector.com>
2019-02-08 20:39:36 -0600

    Fix infinite loop in index("") (fix #1815)

--- src/jv.c
+++ src/jv.c
@@ -658,6 +658,8 @@ jv jv_string_indexes(jv j, jv k) {
   int idxlen = jv_string_length_bytes(jv_copy(k));
   jv a = jv_array();
 
+  if (idxlen == 0)
+    return a;
   p = jstr;
   while ((p = _jq_memmem(p, (jstr + jlen) - p, idxstr, idxlen)) != NULL) {
     a = jv_array_append(a, jv_number(p - jstr));
