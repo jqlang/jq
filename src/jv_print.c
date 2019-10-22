7c047a2b56c022e890667c0b66bea0f753ab67c5
bb010g <me@bb010g.com>
2019-10-21 18:57:07 -0700

    Factor out jvp_dump_char from jvp_dump_string

--- src/jv_print.c
+++ src/jv_print.c
@@ -115,9 +115,68 @@ static void put_indent(int n, int flags, FILE* fout, jv* strout, int T) {
   }
 }
 
+static void jvp_dump_char(int c, const char* cstart, const char* cend,
+    int ascii_only, char buf[static 32], FILE* F, jv* S, int T) {
+  // sizeof(buf) cannot be used, despite the size expression & guarantees
+  const int bufsize = 32;
+  assert(c != -1);
+  int unicode_escape = 0;
+  if (0x20 <= c && c <= 0x7E) {
+    // printable ASCII
+    if (c == '"' || c == '\\') {
+      put_char('\\', F, S, T);
+    }
+    put_char(c, F, S, T);
+  } else if (c < 0x20 || c == 0x7F) {
+    // ASCII control character
+    switch (c) {
+    case '\b':
+      put_char('\\', F, S, T);
+      put_char('b', F, S, T);
+      break;
+    case '\t':
+      put_char('\\', F, S, T);
+      put_char('t', F, S, T);
+      break;
+    case '\r':
+      put_char('\\', F, S, T);
+      put_char('r', F, S, T);
+      break;
+    case '\n':
+      put_char('\\', F, S, T);
+      put_char('n', F, S, T);
+      break;
+    case '\f':
+      put_char('\\', F, S, T);
+      put_char('f', F, S, T);
+      break;
+    default:
+      unicode_escape = 1;
+      break;
+    }
+  } else {
+    if (ascii_only) {
+      unicode_escape = 1;
+    } else {
+      put_buf(cstart, cend - cstart, F, S, T);
+    }
+  }
+  if (unicode_escape) {
+    if (c <= 0xffff) {
+      snprintf(buf, bufsize, "\\u%04x", c);
+    } else {
+      c -= 0x10000;
+      snprintf(buf, bufsize, "\\u%04x\\u%04x",
+              0xD800 | ((c & 0xffc00) >> 10),
+              0xDC00 | (c & 0x003ff));
+    }
+    put_str(buf, F, S, T);
+  }
+}
 
 static void jvp_dump_string(jv str, int flags, FILE* F, jv* S) {
   assert(jv_get_kind(str) == JV_KIND_STRING);
+  const int ascii_only = flags & JV_PRINT_ASCII;
   const int T = flags & JV_PRINT_ISATTY;
   const char* i = jv_string_value(str);
   const char* end = i + jv_string_length_bytes(jv_copy(str));
@@ -126,59 +185,7 @@ static void jvp_dump_string(jv str, int flags, FILE* F, jv* S) {
   char buf[32];
   put_char('"', F, S, T);
   while ((i = jvp_utf8_next((cstart = i), end, &c))) {
-    assert(c != -1);
-    int unicode_escape = 0;
-    if (0x20 <= c && c <= 0x7E) {
-      // printable ASCII
-      if (c == '"' || c == '\\') {
-        put_char('\\', F, S, T);
-      }
-      put_char(c, F, S, T);
-    } else if (c < 0x20 || c == 0x7F) {
-      // ASCII control character
-      switch (c) {
-      case '\b':
-        put_char('\\', F, S, T);
-        put_char('b', F, S, T);
-        break;
-      case '\t':
-        put_char('\\', F, S, T);
-        put_char('t', F, S, T);
-        break;
-      case '\r':
-        put_char('\\', F, S, T);
-        put_char('r', F, S, T);
-        break;
-      case '\n':
-        put_char('\\', F, S, T);
-        put_char('n', F, S, T);
-        break;
-      case '\f':
-        put_char('\\', F, S, T);
-        put_char('f', F, S, T);
-        break;
-      default:
-        unicode_escape = 1;
-        break;
-      }
-    } else {
-      if (flags & JV_PRINT_ASCII) {
-        unicode_escape = 1;
-      } else {
-        put_buf(cstart, i - cstart, F, S, T);
-      }
-    }
-    if (unicode_escape) {
-      if (c <= 0xffff) {
-        snprintf(buf, sizeof(buf), "\\u%04x", c);
-      } else {
-        c -= 0x10000;
-        snprintf(buf, sizeof(buf), "\\u%04x\\u%04x",
-                0xD800 | ((c & 0xffc00) >> 10),
-                0xDC00 | (c & 0x003ff));
-      }
-      put_str(buf, F, S, T);
-    }
+    jvp_dump_char(c, cstart, i, ascii_only, buf, F, S, T);
   }
   assert(c != -1);
   put_char('"', F, S, T);
