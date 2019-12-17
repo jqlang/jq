fa3733cc4c001106847d2591f00abd401ce3e940
Alanscut <wp_scut@163.com>
2019-12-17 19:29:04 +0800

    fix typos

--- src/decNumber/decNumberLocal.h
+++ src/decNumber/decNumberLocal.h
@@ -174,7 +174,7 @@
 
 
   /* ---------------------------------------------------------------- */
-  /* Definitions for arbitary-precision modules (only valid after     */
+  /* Definitions for arbitrary-precision modules (only valid after     */
   /* decNumber.h has been included)                                   */
   /* ---------------------------------------------------------------- */
 
