9c9013b0ea8a5ac9961d720be0b1aca42e7eb8b3
Nicolas Williams <nico@cryptonector.com>
2020-01-29 11:33:37 -0600

    Fix #2051 -- iterate arrays backwards in path ctx
    In case a path gets deleted, we should iterate arrays backwards in
    path/1 context.

--- src/execute.c
+++ src/execute.c
@@ -901,7 +901,7 @@ jv jq_next(jq_state *jq) {
         is_last = idx == len - 1;
         if (keep_going) {
           key = jv_number(idx);
-          value = jv_array_get(jv_copy(container), idx);
+          value = jv_array_get(jv_copy(container), jq->subexp_nest ? idx : len - (idx + 1));
         }
       } else if (jv_get_kind(container) == JV_KIND_OBJECT) {
         if (opcode == EACH || opcode == EACH_OPT) idx = jv_object_iter(container);
