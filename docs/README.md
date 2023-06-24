28b1ee8408797d0bf47ddcf0780c79778c983deb
itchyny <itchyny@cybozu.co.jp>
2023-06-24 13:05:39 +0900

    Redesign website
    * Bump up Bootstrap to v5.3.1, Bootstrap Icon to v1.10.5.
    * Use autoComplete.js to drop dependency on jQuery and typeahead.js.
    * Support dark mode.
    * New svg logo and icon with responsive color mode support.
    * Normalize section ids to lower kebab-case for easiness of linking.
    * Use relative paths for links for local development (--root /output).
    * Various markup cleanups and accessibility improvements.

--- docs/README.md
+++ docs/README.md
@@ -11,16 +11,16 @@ need `python3` and `pipenv`. You can install `pipenv` like so:
 
 Though, you may need to say `pip3` instead, depending on your system. Once
 you have `pipenv` installed, you can install the dependencies by running
-`pipenv sync` from the `docs` directory.
+`pipenv sync` from the `docs/` directory.
 
 Also, you may need to run `virtualenv -p /usr/bin/python3 venv/` and
 then `source venv/bin/activate`, and only then `pipenv sync`.
 
 Once this is done, rerun `./configure` in the jq root directory and then
-the Makefile will be able to generate the jq manpage.  You can also just
-run `pipenv run build_manpage.py` in the `docs` directory to build the
-`jq.1` page manually, and `pipenv run build_mantests.py` to build the
-contents of `tests/man.test` and `tests/manonig.test`.
+the `Makefile` will be able to generate the jq manpage.  You can just run
+`make jq.1` to build the manpage manually, and `make tests/man.test` to
+update the manual tests.
 
-To build the website, run `pipenv run ./build_website.py` from inside
-the `docs` directory.
+To build the website, run `pipenv run python3 build_website.py --root /output`
+in the `docs/` directory. To serve them locally, you can run
+`python3 -m http.server`.
--- docs/build_website.py
+++ docs/build_website.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import glob
 import itertools
 from jinja2 import Environment, FileSystemLoader, select_autoescape, pass_context
@@ -9,6 +10,10 @@ import os.path
 import re
 import shutil
 import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root', default='/jq')
+args = parser.parse_args()
 
 env = Environment(
     loader=FileSystemLoader('templates'),
@@ -21,40 +26,51 @@ def load_yml_file(fn):
         return yaml.safe_load(f)
 
 
+env.globals['url'] = 'https://jqlang.github.io/jq'
+env.globals['root'] = args.root
+
 env.filters['search_id'] = lambda input: input.replace(r'`', '')
-env.filters['section_id'] = lambda input: re.sub(r"[^a-zA-Z0-9_]", '', input)
-env.filters['entry_id'] = lambda input: re.sub(r"[ `]", '', input)
+env.filters['section_id'] = lambda input: re.sub(
+    r'[^-a-zA-Z0-9_]', '', input.replace(' ', '-')).lower()
+env.filters['entry_id'] = lambda input: re.sub(
+    r'^(split|first-last-nth)$',
+    r'\1' + ('-1' if ';' not in input else '-2'),  # avoid id conflict
+    re.sub(
+        r'\b([^-]+)(?:-\1)+\b',
+        r'\1',  # e.g. range-range-range -> range
+        re.sub(r' ?/ ?|,? ', '-',
+               re.sub(r'[`;]|: .*|\(.*?\)| \[.+\]', '', input)))).lower()
 env.filters['markdownify'] = lambda input: Markup(markdown(input))
-env.filters['no_paragraph'] = lambda input: Markup(re.sub(r"</?p>", '', input))
+env.filters['no_paragraph'] = lambda input: Markup(re.sub(r'</?p>', '', input))
 
 env.globals['unique_id'] = pass_context(
     lambda ctx: str(next(ctx['unique_ctr'])))
 
-env.globals.update(load_yml_file('site.yml'))
 
-env.globals['navigation'] = ['tutorial', 'download', 'manual']
+def raise_handler(message):
+    raise Exception(message)
 
 
-def generate_file(env, fname='content/1.tutorial/default.yml'):
+env.globals['raise'] = raise_handler
+
+
+def generate_file(env, fname):
     path, base = os.path.split(fname)
     path = os.path.relpath(path, 'content')
     if path == '.':
         path = ''
-        slug = 'index'
         permalink = ''
     else:
-        slug = os.path.basename(path)
         permalink = path + '/'
 
     output_dir = os.path.join('output', path)
     output_path = os.path.join(output_dir, 'index.html')
 
-    template_name = re.sub(r".yml$", '.html.j2', base)
+    template_name = re.sub(r'.yml$', '.html.j2', base)
 
     ctx = load_yml_file(fname)
     ctx.update(unique_ctr=itertools.count(1),
                permalink=permalink,
-               slug=slug,
                navitem=path)
     os.makedirs(output_dir, exist_ok=True)
     env.get_template(template_name).stream(ctx).dump(output_path,
@@ -72,6 +88,7 @@ def copy_public_files(root=''):
             shutil.copyfile(f.path, dst)
 
 
+os.makedirs('output', exist_ok=True)
 copy_public_files()
 
 for fn in glob.glob('content/**/*.yml', recursive=True):
