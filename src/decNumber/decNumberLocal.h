14e5e630edb85ffeb8a1eb5549f6c1b6e1b6bb27
itchyny <itchyny@cybozu.co.jp>
2023-07-12 08:20:02 +0900

    Revert unexpected changes in decNumber code (ref #2031)

--- src/decNumber/decNumberLocal.h
+++ src/decNumber/decNumberLocal.h
@@ -174,7 +174,7 @@
 
 
   /* ---------------------------------------------------------------- */
-  /* Definitions for arbitrary-precision modules (only valid after     */
+  /* Definitions for arbitary-precision modules (only valid after     */
   /* decNumber.h has been included)                                   */
   /* ---------------------------------------------------------------- */
 
