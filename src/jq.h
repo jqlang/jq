6b82dd3c47c5eee8a7fcca063c683caafd5cdd9a
Tyler Rockwood <rockwotj@users.noreply.github.com>
2023-11-28 15:50:25 -0600

    libjq: extern C for C++
    If using libjq from C++ it would be nice to not need to do this at the
    import site, so just extern "C" to the public headers for libjq
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/jq.h
+++ src/jq.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include "jv.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum {
   JQ_DEBUG_TRACE = 1,
   JQ_DEBUG_TRACE_DETAIL = 2,
@@ -68,5 +72,9 @@ jv jq_util_input_get_current_filename(jq_state*);
 jv jq_util_input_get_current_line(jq_state*);
 
 int jq_set_colors(const char *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !JQ_H */
