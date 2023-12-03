693a7261e5ebccaf0c74fde0ac48700ba3d52f17
DavidKorczynski <david@adalogics.com>
2023-12-03 02:53:49 +0000

    jq_fuzz_execute: fix memory leak
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tests/jq_fuzz_execute.cpp
+++ tests/jq_fuzz_execute.cpp
@@ -27,9 +27,11 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
           jv_free(actual);
         }
         jv_free(next);
+      } else {
+        // Only free if input is invalid as otherwise jq_teardown
+        // frees it.
+        jv_free(input);
       }
-
-      // Do not free "input" as this is handled by jq_teardown.
     }
   }
   jq_teardown(&jq);
