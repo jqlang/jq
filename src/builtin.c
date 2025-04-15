82923642ec2054f2eb88ea0fc3867de3728ad6cd
Henrik Holst <henrik.holst@millistream.com>
2025-04-15 20:12:13 +0200

    Changed from ssv to scsv

--- src/builtin.c
+++ src/builtin.c
@@ -558,7 +558,7 @@ static jv f_format(jq_state *jq, jv input, jv fmt) {
   } else if (!strcmp(fmt_s, "text")) {
     jv_free(fmt);
     return f_tostring(jq, input);
-  } else if (!strcmp(fmt_s, "csv") || !strcmp(fmt_s, "ssv") || !strcmp(fmt_s, "tsv")) {
+  } else if (!strcmp(fmt_s, "csv") || !strcmp(fmt_s, "scsv") || !strcmp(fmt_s, "tsv")) {
     const char *quotes, *sep, *escapings;
     const char *msg;
     if (!strcmp(fmt_s, "csv")) {
@@ -566,8 +566,8 @@ static jv f_format(jq_state *jq, jv input, jv fmt) {
       quotes = "\"";
       sep = ",";
       escapings = "\"\"\"\0";
-    } else if (!strcmp(fmt_s, "ssv")) {
-      msg = "cannot be ssv-formatted, only array";
+    } else if (!strcmp(fmt_s, "scsv")) {
+      msg = "cannot be scsv-formatted, only array";
       quotes = "\"";
       sep = ";";
       escapings = "\"\"\"\0";
