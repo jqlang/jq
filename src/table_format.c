16c55cde8e5e808714847e31bb01fb4a74afd3c9
yertto <yertto@gmail.com>
2025-06-01 12:13:22 +1000

    Add table_format stub

---
+++ src/table_format.c
@@ -0,0 +1,6 @@
+#include "table_format.h"
+
+jv format_table(jq_state *jq, jv input) {
+  // TODO: Implement real table formatting here
+  return jv_string("table formatting not implemented");
+}
---
+++ src/table_format.h
@@ -0,0 +1,9 @@
+#ifndef TABLE_FORMAT_H
+#define TABLE_FORMAT_H
+
+#include "jq.h"
+#include "jv.h"
+
+jv format_table(jq_state *jq, jv input);
+
+#endif // TABLE_FORMAT_H
