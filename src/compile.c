99c8a083977a6fab1a3fed538551f65fe10cc5fa
zhanglangning <zln1905391059@163.com>
2026-08-10 22:18:53 +0800

    Fix destructuring in path expressions

--- src/compile.c
+++ src/compile.c
@@ -998,7 +998,14 @@ block gen_destructure(block var, block matchers, block body) {
     top = BLOCK(top, gen_op_simple(DUP));
   }
 
-  return BLOCK(top, gen_subexp(var), gen_op_simple(POP), bind_alternation_matchers(matchers, body));
+  return BLOCK(top,
+               gen_op_simple(SUBEXP_BEGIN),
+               gen_subexp(var),
+               gen_op_simple(POP),
+               bind_alternation_matchers(matchers,
+                                         BLOCK(gen_op_simple(SUBEXP_END),
+                                               gen_op_simple(POP),
+                                               body)));
 }
 
 // Like gen_var_binding(), but bind `break`'s wildcard unbound variable
